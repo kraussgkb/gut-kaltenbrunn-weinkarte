0a770f3b919c58b1ce198f8be5bc600ebbf6e01c
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-18 13:50:56 +0200

    Add verified AIX and Terlan rosé images

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -54,6 +54,9 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "RO905-1-23": "Weinbilder/RO905-1-23.webp?v=master-20260918-4",
   "RO906-3-23": "Weinbilder/RO906-3-23.webp?v=master-20260918-4",
   "RO906-4-23": "Weinbilder/RO906-4-23.webp?v=master-20260918-4",
+  "RO907-1-25": "Weinbilder/RO907-1-25.webp?v=master-20260918-5",
+  "RO907-2-25": "Weinbilder/RO907-2-25.webp?v=master-20260918-5",
+  "RO909-1-25": "Weinbilder/RO909-1-25.webp?v=master-20260918-5",
   "WÖ409-1-22": "Weinbilder/WÖ409-1-22.webp?v=master-20260918-1",
   "RI132-1-22": "Weinbilder/RI132-1-22.jpeg"
 });
