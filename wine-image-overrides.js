1acf8f4d1ac9f22f060578e6afcaff9150459b0a
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-18 16:23:29 -0700

    Add verified Loimer LOIS 2024 image

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -46,6 +46,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WÖ406-1-21": "Weinbilder/WÖ406-1-21.webp?v=master-20260918-13",
   "WÖ408-1-24": "Weinbilder/WÖ408-1-24.webp?v=master-20260918-10",
   "WÖ410-1-25": "Weinbilder/WÖ410-1-25.webp?v=master-20260919-1",
+  "WÖ425-1-24": "Weinbilder/WÖ425-1-24.webp?v=master-20260919-2",
   "WD374-0-24": "Weinbilder/WD374-0-24.webp",
   "WÖ457-0-24": "Weinbilder/WÖ457-0-24.webp",
   "WD375-0-24": "Weinbilder/WD375-0-24.webp",
