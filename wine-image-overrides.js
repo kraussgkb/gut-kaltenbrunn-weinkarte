41543a104ce7eb48de627ac94136363838d8fc5f
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-18 20:31:59 +0200

    Add verified Laurent-Perrier Cuvée Rosé bottle image

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -9,7 +9,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "CH803-1-NV": "Weinbilder/CH803-1-NV.webp",
   "CH804-1-NV": "Weinbilder/CH804-1-NV.webp",
   "CH804-2-NV": "Weinbilder/CH804-2-NV.webp",
-  "CH805-1-NV": "Weinbilder/CH805-1-NV.webp",
+  "CH805-1-NV": "Weinbilder/CH805-1-NV.webp?v=master-20260918-6",
   "CH805-2-NV": "Weinbilder/CH805-2-NV.webp",
   "CH805-3-NV": "Weinbilder/CH805-3-NV.webp",
   "CH806-1-NV": "Weinbilder/CH806-1-NV.webp",
