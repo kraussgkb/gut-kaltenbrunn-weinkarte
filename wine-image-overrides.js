94afd5d519d313c46cd2d81609e24f6537146e63
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-18 23:02:52 -0700

    Add verified Wohlmuth Steinriegl 2023 image

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -56,6 +56,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WÖ430-1-22": "Weinbilder/WÖ430-1-22.webp?v=master-20260919-8",
   "WÖ431-1-23": "Weinbilder/WÖ431-1-23.webp?v=master-20260919-8",
   "WÖ458-1-25": "Weinbilder/WÖ458-1-25.webp?v=master-20260919-9",
+  "WÖ461-1-23": "Weinbilder/WÖ461-1-23.webp?v=master-20260919-10",
   "WD374-0-24": "Weinbilder/WD374-0-24.webp",
   "WÖ457-0-24": "Weinbilder/WÖ457-0-24.webp",
   "WD375-0-24": "Weinbilder/WD375-0-24.webp",
