0185257e873c3fa4a3a68ca2c342df08a84c5e22
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 15:21:37 -0700

    Add verified Terlan Winkl and IDDA white wine images

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -175,5 +175,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WI516-1-22": "Weinbilder/WI516-1-22.webp?v=master-20260920-76",
   "WI516-1-24": "Weinbilder/WI516-1-24.webp?v=master-20260920-77",
   "WI520-1-21": "Weinbilder/WI520-1-21.webp?v=master-20260920-78",
-  "WI522-1-22": "Weinbilder/WI522-1-22.webp?v=master-20260920-79"
+  "WI522-1-22": "Weinbilder/WI522-1-22.webp?v=master-20260920-79",
+  "WI523-1-25": "Weinbilder/WI523-1-25.webp?v=master-20260920-80",
+  "WI524-1-23": "Weinbilder/WI524-1-23.webp?v=master-20260920-81"
 });
