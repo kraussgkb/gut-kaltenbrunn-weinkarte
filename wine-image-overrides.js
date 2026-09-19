671ebdacd5ffd3e39af0bd35753e6c5fe7a3e655
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 00:20:27 -0700

    Add verified Knoll Kreutles Federspiel 2024 image

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -46,6 +46,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WÖ406-1-21": "Weinbilder/WÖ406-1-21.webp?v=master-20260918-13",
   "WÖ408-1-24": "Weinbilder/WÖ408-1-24.webp?v=master-20260918-10",
   "WÖ410-1-25": "Weinbilder/WÖ410-1-25.webp?v=master-20260919-1",
+  "WÖ416-1-24": "Weinbilder/WÖ416-1-24.webp?v=master-20260919-18",
   "WÖ417-1-24": "Weinbilder/WÖ417-1-24.webp?v=master-20260919-17",
   "WÖ420-1-21": "Weinbilder/WÖ420-1-21.webp?v=master-20260919-5",
   "WÖ421-1-24": "Weinbilder/WÖ421-1-24.webp?v=master-20260919-3",
