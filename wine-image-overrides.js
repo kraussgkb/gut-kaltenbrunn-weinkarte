d9e19cea977bc44d66ff043a622ec5bc6cf5985d
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 15:03:50 -0700

    Add verified Ornellaia and Terlan white wine images

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -169,5 +169,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WI506-1-21": "Weinbilder/WI506-1-21.webp?v=master-20260919-70",
   "WI507-1-20": "Weinbilder/WI507-1-20.webp?v=master-20260919-71",
   "WI508-1-21": "Weinbilder/WI508-1-21.webp?v=master-20260919-72",
-  "WI509-1-20": "Weinbilder/WI509-1-20.webp?v=master-20260919-73"
+  "WI509-1-20": "Weinbilder/WI509-1-20.webp?v=master-20260919-73",
+  "WI514-1-22": "Weinbilder/WI514-1-22.webp?v=master-20260920-74",
+  "WI515-1-22": "Weinbilder/WI515-1-22.webp?v=master-20260920-75"
 });
