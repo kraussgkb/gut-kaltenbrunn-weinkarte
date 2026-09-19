747b4f7e28f53cc1f161715ca0d78afb4356b6d0
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 06:07:21 -0700

    Add verified WD528-1-24 bottle image

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -81,6 +81,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "WD374-0-24": "Weinbilder/WD374-0-24.webp",
   "WÖ457-0-24": "Weinbilder/WÖ457-0-24.webp",
   "WD375-0-24": "Weinbilder/WD375-0-24.webp",
+  "WD528-1-24": "Weinbilder/WD528-1-24.webp?v=master-20260919-1",
   "RD4055-0-22": "Weinbilder/RD4055-0-22.webp",
   "WF606-0-18": "Weinbilder/WF606-0-18.webp",
   "RO901-1-24": "Weinbilder/RO901-1-24.webp?v=master-20260918-3",
