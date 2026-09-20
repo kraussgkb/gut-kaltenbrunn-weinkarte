fbfd942afab0c655b886de3fba2466a9a86b30ce
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 23:05:34 -0700

    Add verified Caberlot and Ilatraia bottle images

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -207,5 +207,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "RI120-1-22": "Weinbilder/RI120-1-22.webp?v=master-20260920-107",
   "RI121-1-15": "Weinbilder/RI121-1-15.webp?v=master-20260920-108",
   "RI123-1-19": "Weinbilder/RI123-1-19.webp?v=master-20260920-109",
-  "RI124-1-19": "Weinbilder/RI124-1-19.webp?v=master-20260920-111"
+  "RI124-1-19": "Weinbilder/RI124-1-19.webp?v=master-20260920-111",
+  "RI125-1-21": "Weinbilder/RI125-1-21.webp?v=master-20260920-112",
+  "RI127-1-21": "Weinbilder/RI127-1-21.webp?v=master-20260920-113"
 });
