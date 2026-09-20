b3abbbb7b8874c509229d5d709b61cc5d3c6fb29
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-20 08:46:23 -0700

    Add verified Vega Sicilia 1989 and 1990 bottle images

--- wine-image-overrides.js
+++ wine-image-overrides.js
@@ -257,5 +257,7 @@ window.WINE_IMAGE_OVERRIDES = Object.freeze({
   "RI139-1-16": "Weinbilder/RI139-1-16.webp?v=master-20260920-158",
   "RI142-1-19": "Weinbilder/RI142-1-19.webp?v=master-20260920-159",
   "RW501-1-16": "Weinbilder/RW501-1-16.webp?v=master-20260920-160",
-  "RW505-1-90": "Weinbilder/RW505-1-90.webp?v=master-20260920-161"
+  "RW505-1-90": "Weinbilder/RW505-1-90.webp?v=master-20260920-161",
+  "RW511-1-90": "Weinbilder/RW511-1-90.webp?v=master-20260920-162",
+  "RW511-1-89": "Weinbilder/RW511-1-89.webp?v=master-20260920-163"
 });
