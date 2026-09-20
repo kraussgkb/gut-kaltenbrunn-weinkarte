8721a23522e956bdcd5f199ec8c34e0adfe33a25
kraussgkb <d.krauss@kaefer-gut-kaltenbrunn.de>
2026-09-19 23:51:18 -0700

    Load temporary order test

--- insight.js
+++ insight.js
@@ -12,3 +12,4 @@ base.onload=()=>{
 };
 document.head.appendChild(base);
 })();
+const orderTest=document.createElement('script');orderTest.src='order-test.js?v=20260920-0851';orderTest.async=false;document.head.appendChild(orderTest);
